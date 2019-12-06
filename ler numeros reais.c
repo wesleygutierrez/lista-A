7438c2f97e06a2583f9ab4f7d78e783436efaf92
wesley gutierrez <57546607+wesleygutierrez@users.noreply.github.com>
2019-12-05 21:45:51 -0300

    num. reais

---
+++ ler numeros reais.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	float valor,valor1;
+	
+	printf("digite o numero 1:");
+	scanf("%f",&valor);
+	printf("digite o numero 2:");
+	scanf("%f",&valor1);
+	printf("numero reais:%f %f", valor,valor1);
+}
