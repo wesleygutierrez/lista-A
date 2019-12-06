435cbc4c702f24a2d67c689ee6115b580cc30096
wesley gutierrez <57546607+wesleygutierrez@users.noreply.github.com>
2019-12-05 21:47:43 -0300

    nome

---
+++ imprime o nome.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void ) {
+	char nome[100];
+	
+	printf("digite seu nome: \n");
+	scanf("%s",&nome);
+	printf("\n%s",nome);
+	
+}
